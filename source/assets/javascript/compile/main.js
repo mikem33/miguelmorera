2ac1aad481d534bfd22fe629514b714d2dc4c871
Miguel Morera <info@miguelmorera.com>
2019-06-16 19:58:00 +0200

    Lazy load functionality has been added. A function to add a CSS variable to the root to calculate the width of the scrollbar has been added. This is not being used at this point.

--- source/assets/javascript/compile/main.js
+++ source/assets/javascript/compile/main.js
@@ -5,3 +5,75 @@ jQuery(document).ready(function($) {
     }
     
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+    let active = false;
+
+    const lazyLoad = function() {
+        if (active === false) {
+            active = true;
+
+            setTimeout(function() {
+                lazyImages.forEach(function(lazyImage) {
+                    if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+                        lazyImage.src = lazyImage.dataset.src;
+                        // lazyImage.srcset = lazyImage.dataset.srcset;
+                        lazyImage.classList.remove("lazy");
+                        lazyImage.classList.add("lazy--loaded");
+
+                        lazyImages = lazyImages.filter(function(image) {
+                            return image !== lazyImage;
+                        });
+
+                        if (lazyImages.length === 0) {
+                            document.removeEventListener("scroll", lazyLoad);
+                            window.removeEventListener("resize", lazyLoad);
+                            window.removeEventListener("orientationchange", lazyLoad);
+                        }
+                    }
+                });
+
+                active = false;
+            }, 200);
+        }
+    };
+
+    document.addEventListener("scroll", lazyLoad);
+    window.addEventListener("resize", lazyLoad);
+    window.addEventListener("orientationchange", lazyLoad);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
+
+    if ("IntersectionObserver" in window) {
+        let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    entry.target.style.backgroundImage = "url('" + entry.target.dataset.src + "')";
+                    entry.target.classList.add("visible");
+                    lazyBackgroundObserver.unobserve(entry.target);
+                }
+            });
+        });
+
+        lazyBackgrounds.forEach(function(lazyBackground) {
+            lazyBackgroundObserver.observe(lazyBackground);
+        });
+    }
+});
+
+// Get the scrollbar width
+const getScrollBarWidth = () => window.innerWidth - document.documentElement.getBoundingClientRect().width;
+
+// Assign the obtained data width to a CSS Variables.
+const cssScrollBarWidth = () => document.documentElement.style.setProperty('--scrollbar', `${getScrollBarWidth()}px`);
+let cssScrollBarClass = () => document.documentElement.classList.add('scrollbar--calculated');
+
+// Assign this variable when the page loads.
+addEventListener('load', cssScrollBarWidth);
+addEventListener('load', cssScrollBarClass);
+
+// Reassign the variable when the window resizes.
+addEventListener('resize', cssScrollBarWidth);
