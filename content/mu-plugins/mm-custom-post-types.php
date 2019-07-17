8843a6ca8bc6eddf7e449034e6666e9290e91658
Miguel Morera <info@miguelmorera.com>
2019-07-17 20:13:18 +0200

    MU Plugin for register CPTs. Modified the original Mark Jaquith MU Plugin to have a nice looking title in the plugin list.

---
+++ content/mu-plugins/mm-custom-post-types.php
@@ -0,0 +1,115 @@
+<?php
+/*
+Plugin Name: MU Custom Post Types
+Plugin URI: http://www.miguelmorera.com
+Description: MU-Plugin for Custom Post Types. Based on Van Patten Media code. (http://www.vanpattenmedia.com/)
+Author: Miguel Morera
+Version: 1.0
+Author URI: https://www.miguelmorera.com/
+*/
+/**
+ *
+ * Register post types
+ *
+ */
+if ( !post_type_exists('work') ) {
+    function register_work_type() {
+        $label_singular = __( 'Work', 'miguelmorera' );
+        $label_plural   = __( 'Works', 'miguelmorera' );
+        register_post_type(
+            'work',
+            array(
+                'label'           => $label_plural,
+                'description'     => '',
+                'menu_icon'       => 'dashicons-hammer',
+                'menu_position'   => 4,
+                'public'          => true,
+                'show_ui'         => true,
+                'show_in_menu'    => true,
+                'capability_type' => 'post',
+                'hierarchical'    => false,
+                'query_var'       => true,
+                'has_archive'     => true,
+                'rewrite' => array(
+                    'slug'       => 'works',
+                    'with_front' => false,
+                ),
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'revisions',
+                    'thumbnail',
+                    'custom-fields',
+                ),
+                'labels' => array (
+                    'name'               => $label_plural,
+                    'singular_name'      => $label_singular,
+                    'menu_name'          => $label_plural,
+                    'add_new'            => __( 'Add New', 'miguelmorera' ),
+                    'add_new_item'       => __( 'Add New ', 'miguelmorera' ) . $label_singular,
+                    'edit'               => __( 'Edit', 'miguelmorera' ),
+                    'edit_item'          => __( 'Edit ', 'miguelmorera' ) . $label_singular,
+                    'new_item'           => __( 'New ', 'miguelmorera' ). $label_singular,
+                    'view'               => __( 'View ', 'miguelmorera' ) . $label_singular,
+                    'view_item'          => __( 'View ', 'miguelmorera' ) . $label_singular,
+                    'search_items'       => __( 'Search ', 'miguelmorera' ) . $label_plural,
+                    'not_found'          => __( 'No ', 'miguelmorera' ) . $label_plural . ' Found',
+                    'not_found_in_trash' => __( 'No ', 'miguelmorera' ) . $label_plural . ' Found in Trash',
+                    'parent'             => __( 'Parent ', 'miguelmorera' ) . $label_singular,
+                )
+            )
+        );
+    }
+    add_action('init', 'register_work_type');
+}
+
+if ( !post_type_exists('comic') ) {
+    function register_comic_type() {
+        $label_singular = __( 'Comic', 'miguelmorera' );
+        $label_plural   = __( 'Comics', 'miguelmorera' );
+        register_post_type(
+            'comic',
+            array(
+                'label'           => $label_plural,
+                'description'     => '',
+                'menu_icon'       => 'dashicons-format-image',
+                'menu_position'   => 5,
+                'public'          => true,
+                'show_ui'         => true,
+                'show_in_menu'    => true,
+                'capability_type' => 'post',
+                'hierarchical'    => false,
+                'query_var'       => true,
+                'has_archive'     => true,
+                'rewrite' => array(
+                    'slug'       => 'comics',
+                    'with_front' => false,
+                ),
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'revisions',
+                    'thumbnail',
+                    'custom-fields',
+                ),
+                'labels' => array (
+                    'name'               => $label_plural,
+                    'singular_name'      => $label_singular,
+                    'menu_name'          => $label_plural,
+                    'add_new'            => __( 'Add New', 'miguelmorera' ),
+                    'add_new_item'       => __( 'Add New ', 'miguelmorera' ) . $label_singular,
+                    'edit'               => __( 'Edit', 'miguelmorera' ),
+                    'edit_item'          => __( 'Edit ', 'miguelmorera' ) . $label_singular,
+                    'new_item'           => __( 'New ', 'miguelmorera' ). $label_singular,
+                    'view'               => __( 'View ', 'miguelmorera' ) . $label_singular,
+                    'view_item'          => __( 'View ', 'miguelmorera' ) . $label_singular,
+                    'search_items'       => __( 'Search ', 'miguelmorera' ) . $label_plural,
+                    'not_found'          => __( 'No ', 'miguelmorera' ) . $label_plural . ' Found',
+                    'not_found_in_trash' => __( 'No ', 'miguelmorera' ) . $label_plural . ' Found in Trash',
+                    'parent'             => __( 'Parent ', 'miguelmorera' ) . $label_singular,
+                )
+            )
+        );
+    }
+    add_action('init', 'register_comic_type');
+}
--- content/mu-plugins/register-theme-directory.php
+++ content/mu-plugins/register-theme-directory.php
@@ -1,3 +1,13 @@
 <?php
+/*
+Plugin Name: Register Theme Directory
+Plugin URI: https://www.miguelmorera.com
+Description: This is for be capable of using the default WordPress themes in the WordPress Skeleton by Mark Jaquith. (https://markjaquith.com/)
+Author: Miguel Morera
+Version: 1.0
+Author URI: https://www.miguelmorera.com/
+*/
 
 register_theme_directory( ABSPATH . 'wp-content/themes/' );
+
+?>
