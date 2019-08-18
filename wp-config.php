0291f416b46276cd50e33a6801e12f76203a5f9d
Miguel Morera <info@miguelmorera.com>
2019-08-18 12:45:27 +0200

    Changes in wp-config.php to match with the new site config settings.

--- wp-config.php
+++ wp-config.php
@@ -2,22 +2,15 @@
 /**
  * Load database info and local development parameters
  */
-if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
-    define( 'WP_LOCAL_DEV', true );
-    include( dirname( __FILE__ ) . '/local-config.php' );
-} else {
-    define( 'WP_LOCAL_DEV', false );
-    define( 'DB_NAME', '%%DB_NAME%%' );
-    define( 'DB_USER', '%%DB_USER%%' );
-    define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
-    define( 'DB_HOST', '%%DB_HOST%%' ); // Probably 'localhost'
-}
+
+include( dirname( __FILE__ ) . '/private/config.php' );
 
 /**
  * Custom Content Directory
  */
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+define( 'ROOT_PATH', dirname( __FILE__ ) );
 
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
@@ -34,12 +27,6 @@ define( 'AUTH_SALT',        'put your unique phrase here' );
 define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
-
-/**
- * Table prefix
- * Change this if you have multiple installs in the same database
- */
-$table_prefix  = 'mm_';
 
 /**
  * Language
@@ -65,12 +52,6 @@ define( 'WP_DEBUG_DISPLAY', false );
  */
 if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
     $memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
-
-/**
- * This can be used to programatically set the stage when deploying (e.g. production, staging)
- */
-define( 'WP_STAGE', '%%WP_STAGE%%' );
-define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
 
 /**
  * Bootstrap WordPress
