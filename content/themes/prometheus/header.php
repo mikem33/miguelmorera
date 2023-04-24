c7facb81353c269667f82b6fd598d2109d5bdf41
Miguel Morera <info@miguelmorera.com>
2023-04-24 21:19:44 +0200

    Removing language switcher from the menu.

--- content/themes/prometheus/header.php
+++ content/themes/prometheus/header.php
@@ -40,7 +40,7 @@
             <div class="nav section space">
                 <div class="content">
                     <?php $menuParameters = array(
-                        'menu'            => 6,
+                        'menu'            => 16,
                         'container'       => 'nav',
                         'container_class' => 'nav__items',
                         'echo'            => false,
@@ -48,7 +48,6 @@
                         'depth'           => 0,
                     ); ?>
                     <?php echo strip_tags(wp_nav_menu( $menuParameters ), '<nav>,<a>' ); ?>
-                    <?php if ( function_exists( 'the_msls' ) ) the_msls(); ?>
                 </div> <!--  /.content -->
                 <p class="nav__colophon">&copy; 2010 - <?php echo date('Y'); ?> Miguel Morera</p>
             </div> <!--  /.nav .section .space -->
