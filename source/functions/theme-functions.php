08c6d4265370ef24ee5853c1404d0c73ddafde22
Miguel Morera <info@miguelmorera.com>
2024-04-12 16:33:22 +0200

    fix: disable WP automatic big image scale

--- source/functions/theme-functions.php
+++ source/functions/theme-functions.php
@@ -245,4 +245,9 @@
     add_filter( 'wpcf7_load_js', '__return_false' );
     add_filter( 'wpcf7_load_css', '__return_false' );
 
+    /**
+     * Disable automatic image scale.
+     */
+    add_filter( 'big_image_size_threshold', '__return_false' );
+
 ?>
